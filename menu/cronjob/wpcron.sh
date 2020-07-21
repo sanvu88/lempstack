d7a0d6f0c16aace47381d85dd0d2df0b35171dce
sanvu88 <quylevhb@gmail.com>
2020-07-21 17:40:13 +0700

    Change command call wordpress cron from wget to curl

--- menu/cronjob/wpcron.sh
+++ menu/cronjob/wpcron.sh
@@ -11,6 +11,7 @@
 for domains in "${WORDPRESS_CRON_DIR}"/* ; do
     if [ -f "${domains}" ]; then
         domain=${domains##*/}
-        wget -q -O - http://"${domain}"/wp-cron.php?doing_wp_cron
+        #wget -q -O - http://"${domain}"/wp-cron.php?doing_wp_cron
+        curl -I -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36" http://"${domain}"/wp-cron.php?doing_wp_cron
     fi
 done
