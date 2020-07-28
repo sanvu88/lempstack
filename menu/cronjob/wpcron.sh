cac5d5fbab10c52171870692c45878233a8c8dda
sanvu88 <quylevhb@gmail.com>
2020-07-28 10:42:50 +0700

    Added Menu Manager WP-CRON

--- menu/cronjob/wpcron.sh
+++ menu/cronjob/wpcron.sh
@@ -6,14 +6,11 @@
 #                Author: Sanvv - HOSTVN Technical                    #
 #                  Website: https://hostvn.vn                        #
 #                                                                    #
-#              Please don't remove copyright. Thank!                 #
-#   Please don't copy under any circumstance for commercial reason!  #
+#              Please do not remove copyright. Thank!                #
+#  Please do not copy under any circumstance for commercial reason!  #
 ######################################################################
 
-for domains in "${WORDPRESS_CRON_DIR}"/* ; do
-    if [ -f "${domains}" ]; then
-        domain=${domains##*/}
-        #wget -q -O - http://"${domain}"/wp-cron.php?doing_wp_cron
-        curl -I -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36" http://"${domain}"/wp-cron.php?doing_wp_cron
-    fi
+for domains in /var/hostvn/wpcron/* ; do
+    domain=${domains##*/}
+    curl -I -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36" http://"${domain}"/wp-cron.php?doing_wp_cron
 done
